cce2716d1b721b6e23457b141bca4b668e187874
drolejan <drolejan@gmail.com>
2021-07-17 13:20:01 -0500

    Primer Ejercicio

--- src/scope/global.js
+++ src/scope/global.js
@@ -9,3 +9,19 @@ const anotherFunction = () =>{
 }
 
 anotherFunction();
+
+//Mala practica aca abajo
+
+const hellowWorld2 = () => {
+    globalVar = 'im global'
+}
+
+hellowWorld2();
+console.log(globalVar);
+
+const anotherFunction = () =>{
+    var localVar = globalVar = 'Im Global';
+}
+
+anotherFunction();
+console.log(globalVar);
